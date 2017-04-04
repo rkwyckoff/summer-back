68c985f05b436a755456ddc8ba2661896e6f557c
Kurt Wyckoff <kurtitservices@gmail.com>
2017-04-04 09:39:46 -0400

    disabled likes for now--wanting to get counter likes working

--- server/controllers/likes.js
+++ server/controllers/likes.js
@@ -4,31 +4,37 @@ const User = require("../models").User;
 const Likes = require("../models").Likes;
 
 
-module.exports = {
-  create (req, res) {
-
-    Likes.findOrCreate({
-      where: {
-        user_id: req.user.id,
-        activity_id: req.params.id
-        //comment_id: req.comment.id
-
-      }
-      //likesnumber: likesnumber++;
-    })
-    .then(likes => res.status(201).send(likes))
-    .catch(error => res.status(400).send(error))
-  },
-
-  listLikes (req, res) {
-     Likes.findAll({
-        where: {
-           activity_id:req.params.id,
-        }
-    })
-     .then(likes => res.status(200).send(likes))
-     .catch(error => res.status(400).send(error));
-  }
-
-
-}
+// module.exports = {
+//   create (req, res) {
+//
+//     Likes.findOrCreate({
+//       where: {
+//         user_id: req.user.id,
+//         activity_id: req.params.id
+//         //comment_id: req.comment.id
+//
+//       }
+//       //likesnumber: likesnumber++;
+//     })
+//     .then(like => {
+//       let activity = like.getActivity()
+//       console.log(activity)
+//       activity.increment('likesnumber')
+//       res.status(201).send(like)
+//     })
+// //    .then(likes => res.status(201).send(likes))
+//     .catch(error => res.status(400).send(error))
+//   },
+//
+//   listLikes (req, res) {
+//      Likes.findAll({
+//         where: {
+//            activity_id:req.params.id,
+//         }
+//     })
+//      .then(likes => res.status(200).send(likes))
+//      .catch(error => res.status(400).send(error));
+//   }
+//
+//
+// }
